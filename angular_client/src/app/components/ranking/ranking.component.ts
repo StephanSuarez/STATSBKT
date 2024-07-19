9be7ea93819eff2ffd7d7dbf75bc86456117e575
daniel mora <daniels224@gmail.com>
2024-07-18 23:08:21 -0500

    se cambió panel administrador

--- angular_client/src/app/components/ranking/ranking.component.ts
+++ angular_client/src/app/components/ranking/ranking.component.ts
@@ -16,33 +16,5 @@ export class RankingComponent {
     // Añade más jugadores según sea necesario
   ];
 
-  sliderImages = [
-    { src: 'assets/imgs/escudos/fondoU.png', caption: 'Caption One' },
-    { src: 'assets/imgs/danielmora.png', caption: 'Caption Two' },
-    { src: 'assets/logo.jpg', caption: 'Caption Three' },
-    { src: 'assets/imgs/escudos/fondoU.png', caption: 'Caption Four' },
-    { src: 'assets/imgs/escudos/fondoU.png', caption: 'Caption Five' }
-  ];
-
-  currentSlide = 0;
-
-  constructor() { }
-
-  ngOnInit(): void { }
-
-  nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.sliderImages.length;
-  }
-
-  prevSlide() {
-    this.currentSlide = (this.currentSlide - 1 + this.sliderImages.length) % this.sliderImages.length;
-  }
-
-  setCurrentSlide(index: number) {
-    this.currentSlide = index;
-  }
-
-  getSlideClass(index: number): string {
-    return index === this.currentSlide ? 'mySlides active' : 'mySlides';
-  }
+  
 }
